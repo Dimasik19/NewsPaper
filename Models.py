0257184d420610c232ccce7d89fae3ffe2745042
Dimasik19 <119976425+Dimasik19@users.noreply.github.com>
2023-05-16 14:43:48 +0300

    Update Models.py

--- Models.py
+++ Models.py
@@ -1,1 +1,16 @@
+from django.db import models
 
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    
+    rating = models.IntegerField()
+
+class Post(models.Model):
+    post = models.OneToOneField (Author, on_delete = models.CASCADE, primary_key = True)
+    post_cat = models.ManyToManyField(Category)
+    
+    type_is_news = models.BooleanField (default = False)
+    date_of_publ = model.DateTimeField (auto_now_add = True)
+    title = models.CharField (max_length = 64)
+    text = model.TextField ()
+    rating = models.IntegerField()   
